e2137eafda83ee3d3111ea1f54920f847cbdc23d
rmd5 <rmd5@hw.ac.uk>
2021-07-18 19:45:48 +0100

    'Add cors'

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express()
 var corsOptions = {
     origin: [
         "http://localhost:3001",
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://strathdontennis.pages.dev"
     ]
 }
 
